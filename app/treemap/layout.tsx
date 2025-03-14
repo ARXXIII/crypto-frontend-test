7cf25223df7591dac2e59e67259ab1f63a90584d
Андрей Романов <human1488pwnz@yandex.ru>
2025-03-14 18:23:37 +0300

    add TreeMapLayout component for structured layout of children elements

---
+++ app/treemap/layout.tsx
@@ -0,0 +1,9 @@
+const TreeMapLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main className="p-3">
+            {children}
+        </main>
+    )
+}
+
+export default TreeMapLayout
