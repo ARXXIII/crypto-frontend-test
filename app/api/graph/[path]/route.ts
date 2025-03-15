2247b09b6de863403bafc097982ebe94bfde2210
Андрей Романов <human1488pwnz@yandex.ru>
2025-03-15 17:47:11 +0300

    feat: add API route to serve graph data based on path parameter

---
+++ app/api/graph/[path]/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import data1 from '@/public/1.json'
+import data2 from '@/public/2.json'
+import data3 from '@/public/3.json'
+
+export async function GET(
+	req: NextRequest,
+	{ params }: { params: { path: string } }
+) {
+	const { path } = await params
+
+	try {
+		if (path) {
+			let response
+			switch (path) {
+				case '1':
+					response = data1
+					break
+				case '2':
+					response = data2
+					break
+				case '3':
+					response = data3
+					break
+				default:
+					throw new Error('Invalid path')
+			}
+
+			const data = response
+
+			return NextResponse.json(data)
+		}
+	} catch (error) {
+		console.error('GRAPH API ERROR', error)
+		return NextResponse.json('Internal server error', { status: 500 })
+	}
+}
